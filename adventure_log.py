6823db5a9593c5a2aeacf7dd59e5b52d7275ece8
Ronak-P99 <rocky101299@gmail.com>
2024-03-19 15:01:29 -0500

    Updated Python file

--- adventure_log.py
+++ adventure_log.py
@@ -1,5 +1,5 @@
 def greet_coding_temple():
-    print("Ahoy, Coding Temple Navigtors! Ready for today's adventure?")
+    print("Hello, Coding Temple Navigtors!")
 
     if __name__ == "__main__":
         greet_coding_temple()
